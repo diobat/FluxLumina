3cdcbfef6610c3c70eefec06c20c4f9bacab4293
Diogo Batista <diogobatista@ua.pt>
2023-09-28 12:24:38 +0200

    Light sources pre-work

--- src/scene/LightSource.cpp
+++ src/scene/LightSource.cpp
@@ -10,6 +10,27 @@ LightSource::LightSource(float intensity, std::array<float, 3> color)  :
     ;
 }
 
+void LightSource::setIntensity(float intensity)
+{
+    _intensity = intensity;
+}
+
+float LightSource::getIntensity() const 
+{
+    return _intensity;
+}
+
+void LightSource::setColor(const std::array<float, 3>& color)
+{
+    _color = color;
+}
+
+const std::array<float, 3>& LightSource::getColor() const
+{
+    return _color;
+}
+
+
 AmbientLight::AmbientLight(float intensity, std::array<float, 3> color) : 
     _intensity(intensity),
     _color(color)
@@ -37,44 +58,5 @@ void AmbientLight::setColor(std::array<float, 3> color)
 const std::array<float, 3>& AmbientLight::getColor() const
 {
     return _color;
-}
-
-
-// Diffuse Light
-DiffuseLight::DiffuseLight(float intensity, std::array<float, 3> color, std::array<float, 3> direction) :
-    _intensity(intensity),
-    _color(color)
-{
-    _direction = Math::normalize(direction);
-}
-
-const std::array<float, 3>& DiffuseLight::getDirection() const
-{
-    return _direction;
-}
-
-void DiffuseLight::setDirection(std::array<float, 3> direction)
-{
-    _direction = direction;
-}
-
-const std::array<float, 3>& DiffuseLight::getColor() const
-{
-    return _color;
-}
-
-void DiffuseLight::setColor(std::array<float, 3> color)
-{
-    _color = color;
-}
-
-float DiffuseLight::getIntensity() const
-{
-    return _intensity;
-}
-
-void DiffuseLight::setIntensity(float intensity)
-{
-    _intensity = intensity;
 }
 
--- src/scene/LightSource.h
+++ src/scene/LightSource.h
@@ -2,6 +2,10 @@
 
 // First-party includes
 #include <scene/sceneObject.h>
+#include "rendering/model.h"
+
+// STD includes
+#include <memory>
 
 
 class LightSource : public SceneObject
@@ -10,15 +14,14 @@ public:
     LightSource(float intensity = 0.5f, std::array<float, 3> color = {1.0f, 1.0f, 1.0f});
 
     void setIntensity(float intensity);
-    float getIntensity();
+    float getIntensity() const;
 
-    void setColor(std::array<float, 3> color);
-    std::array<float, 3> getColor();
+    void setColor(const std::array<float, 3>& color);
+    const std::array<float, 3>& getColor() const;
 
 private:
     float _intensity;
     std::array<float, 3> _color;
-
 };
 
 
@@ -36,31 +39,4 @@ public:
 private:
     float _intensity;
     std::array<float, 3> _color;
-};
-
-class DiffuseLight : public LightSource
-{
-public:
-    DiffuseLight(float intensity = 0.5f, std::array<float, 3> color = {1.0f, 1.0f, 1.0f}, std::array<float, 3> direction = {0.0f, 0.0f, 0.0f});
-
-    const std::array<float, 3>& getDirection() const;
-    void setDirection(std::array<float, 3> direction);
-
-    const std::array<float, 3>& getColor() const;
-    void setColor(std::array<float, 3> color);
-
-    float getIntensity() const;
-    void setIntensity(float intensity);
-
-private:
-
-    std::array<float, 3> _direction;
-    std::array<float, 3> _color;    
-    float _intensity;
-};
-
-class SpecularLight : public LightSource
-{
-
-
 };
