e21ae21c35fd83b2dfd31dfa92fd01fb5e4d0857
Diogo Batista <diogobatista@ua.pt>
2024-01-03 00:13:30 +0100

    and another

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-// First-party includes
-#include "FluxLumina.hpp"
-#include "util/SceneSetup.h"
-// Third-party includes
-#include <memory>
+// // First-party includes
+// #include "FluxLumina.hpp"
+// #include "util/SceneSetup.h"
+// // Third-party includes
+// #include <memory>
 
-int main(void)
-{
-    FluxLumina graphicalEngine(E_RenderStrategy::ForwardShading);
-    scene01_Setup(graphicalEngine);
-    // Begin main loop
-    graphicalEngine.update();
-    return 0;
-}
+// int main(void)
+// {
+//     FluxLumina graphicalEngine(E_RenderStrategy::ForwardShading);
+//     scene01_Setup(graphicalEngine);
+//     // Begin main loop
+//     graphicalEngine.update();
+//     return 0;
+// }
