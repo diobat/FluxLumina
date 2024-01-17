0c16e95fc023b18e221ab750de9c54dbe76a2161
Diogo Batista <diogobatista@ua.pt>
2024-01-17 21:00:47 +0100

    header merge bugfix

--- src/rendering/FluxLumina.cpp
+++ src/rendering/FluxLumina.cpp
@@ -289,3 +289,29 @@ void FluxLumina::setSpotlightRadius(boost::uuids::uuid UUID, float radius)
         light->setCutoff(radius);
     }
 }
+
+void FluxLumina::bindScene(std::shared_ptr<Scene> scene)
+{
+    _scenes.push_back(scene);
+    _frameBuffers->parseNewScene(scene);
+}
+
+void FluxLumina::unbindScene(std::shared_ptr<Scene> sceneToUnbind)
+{
+    _scenes.erase(std::remove_if(_scenes.begin(), _scenes.end(), 
+    [sceneToUnbind](std::shared_ptr<Scene> scene) { 
+        return scene == sceneToUnbind; 
+        }),
+        _scenes.end());
+}
+
+std::shared_ptr<Scene> FluxLumina::getScene(unsigned int sceneIndex) const
+{
+    if(sceneIndex >= _scenes.size())
+    {
+        return nullptr;
+    }
+
+    return _scenes[sceneIndex];
+}
+
